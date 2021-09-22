6cf167a969c77ce73a265ba556a7e8a77deafe0a
= <npo.communitygta@gmail.com>
2021-09-22 00:43:02 -0400

    updated to https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  appUrl: 'http://communitygta.pythonanywhere.com',
+  appUrl: 'https://communitygta.pythonanywhere.com',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  appUrl: 'http://communitygta.pythonanywhere.com',
+  appUrl: 'https://communitygta.pythonanywhere.com',
 };
 
 /*
